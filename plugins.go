f83c7c8a94e44a6b6001561130a96f113b53cc37
Joey Clover <joey.clover@gmail.com>
2018-03-02 23:07:26 +0000

    woah

--- plugins.go
+++ plugins.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strings"
 
@@ -48,24 +47,36 @@ func (ac *allowedCors) Handler() plugin.Handler {
 		})
 
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			cors.AllowAll().ServeHTTP(w, r, hf)
+			corsOptions := cors.Options{
+				Debug: true,
+			}
+
+			if len(ac.allowedHeaders) != 0 {
+				corsOptions.AllowedHeaders = ac.allowedHeaders
+			}
+
+			if len(ac.allowedMethods) != 0 {
+				corsOptions.AllowedMethods = ac.allowedMethods
+			}
+
+			if len(ac.allowedOrigins) != 0 {
+				corsOptions.AllowedOrigins = ac.allowedOrigins
+			}
+
+			cors.New(corsOptions).ServeHTTP(w, r, hf)
 		})
 	}
 }
 
 func (ac *allowedCors) Init(ctx *cli.Context) error {
-	ac.allowedHeaders = ac.parseAllowed(ctx, "cors-allowed-headers")
-	ac.allowedMethods = ac.parseAllowed(ctx, "cors-allowed-methods")
-	ac.allowedOrigins = ac.parseAllowed(ctx, "cors-allowed-origins")
-
-	log.Println(ac.allowedHeaders)
-	log.Println(ac.allowedMethods)
-	log.Println(ac.allowedOrigins)
+	ac.allowedHeaders = ac.parseAllowed(ctx, "cors-allowed-headers", true)
+	ac.allowedMethods = ac.parseAllowed(ctx, "cors-allowed-methods", false)
+	ac.allowedOrigins = ac.parseAllowed(ctx, "cors-allowed-origins", false)
 
 	return nil
 }
 
-func (ac *allowedCors) parseAllowed(ctx *cli.Context, flagName string) []string {
+func (ac *allowedCors) parseAllowed(ctx *cli.Context, flagName string, addLowerVariants bool) []string {
 	fv := ctx.String(flagName)
 
 	// no op
@@ -73,7 +84,23 @@ func (ac *allowedCors) parseAllowed(ctx *cli.Context, flagName string) []string 
 		return nil
 	}
 
-	return strings.Split(fv, ",")
+	list := strings.Split(fv, ",")
+	fixedList := []string{}
+
+	for _, val := range list {
+		trimmedVal := strings.TrimSpace(val)
+		fixedList = append(fixedList, trimmedVal)
+
+		if addLowerVariants {
+			lval := strings.ToLower(trimmedVal)
+
+			if trimmedVal != lval {
+				fixedList = append(fixedList, lval)
+			}
+		}
+	}
+
+	return fixedList
 }
 
 func (ac *allowedCors) String() string {
@@ -82,11 +109,7 @@ func (ac *allowedCors) String() string {
 
 // NewPlugin Creates the CORS Plugin
 func NewPlugin() plugin.Plugin {
-	return &allowedCors{
-		allowedHeaders: []string{},
-		allowedOrigins: []string{},
-		allowedMethods: []string{},
-	}
+	return &allowedCors{}
 }
 
 func init() {
